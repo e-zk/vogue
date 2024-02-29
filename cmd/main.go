7084aefa35da52127d3f18e246f745b3fd3457a3
zak <e-zk@users.noreply.github.com>
2024-02-29 17:06:47 +1000

    cmd: make functional

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,58 @@
 package main
 
 import (
-	"go.zakaria.org/vogue"
+	"flag"
+	"fmt"
 	"log"
+
+	"go.zakaria.org/vogue"
 )
 
+var (
+	lsSeasons bool
+	lsBrands  bool
+	fullSlug  string
+)
+
+func init() {
+	flag.BoolVar(&lsSeasons, "seasons", false, "list all seasons")
+	flag.BoolVar(&lsBrands, "brands", false, "list all brands")
+	flag.StringVar(&fullSlug, "show", "", "print all collection urls from show")
+	flag.Parse()
+}
+
 func main() {
-	log.Printf("starting bruh...")
-	brands, err := vogue.GetBrands()
-	if err != nil {
-		log.Fatal(err)
+	if fullSlug != "" {
+		ss, err := vogue.GetShow(fullSlug)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, slide := range ss.Galleries.Collection.Slides.Slides {
+			fmt.Printf("%s\n", slide.Image.Url)
+		}
+		return
 	}
-	log.Printf("%+v", brands)
+
+	if lsSeasons {
+		ssns, err := vogue.GetSeasons()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, s := range ssns {
+			fmt.Printf("%s\t%s\n", s.Slug, s.Name)
+		}
+	} else if lsBrands {
+		brands, err := vogue.GetBrands()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, b := range brands {
+			fmt.Printf("%s\t%s\n", b.Slug, b.Name)
+		}
+	} else {
+		log.Fatal("huh? what?")
+	}
 }
